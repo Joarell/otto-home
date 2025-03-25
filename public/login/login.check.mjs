63d603388e4c592762b1c28140364a74a3addf26
Joarell <josephjoarell@gmail.com>
2025-03-25 00:52:03 -0300

    build (cors): set the mode cors on login request

--- public/login/login.check.mjs
+++ public/login/login.check.mjs
@@ -65,7 +65,7 @@ async function backEndLoginAuth(userInfo) {
 
 	await fetch (url, {
 		method: "POST",
-		mode: 'no-cors',
+		mode: 'cors',
 		body: USER,
 		headers: {
 			'Content-Type': 'application/json; charset=UTF-8',
