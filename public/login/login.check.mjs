a5c10c709fb2ab25cb81ea09260e76eb79cd62df
Joarell <josephjoarell@gmail.com>
2025-03-28 20:54:07 -0300

    fix (typo): adds the coma on header directives

--- public/login/login.check.mjs
+++ public/login/login.check.mjs
@@ -73,8 +73,8 @@ async function backEndLoginAuth(userInfo) {
 			'Content-Type': 'text/javascript; charset=UTF-8',
 			'Cache-Control': 'max-age=3600, max-stale=1800, min-fresh=3600, only-if-cached',
 			'Vary': 'Accept-Language',
-			'Accept': '*/*'
-			'Accept': 'application/dns-message; charset=UTF-8',
+			'Accept': '*/*',
+			'Accept': 'application/dns-message; charset=UTF-8'
 		},
 	}).then(async res => {
 		console.log(res.headers.getSetCookie())
