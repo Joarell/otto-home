38b5c3481855bd0eee7a8bd15298ec8d48e0b13c
Joarell <josephjoarell@gmail.com>
2025-03-30 16:30:18 -0300

    chore (login): add mode to no-cors

--- public/login/login.check.mjs
+++ public/login/login.check.mjs
@@ -65,11 +65,12 @@ async function backEndLoginAuth(userInfo) {
 	headers.set('Cache-Control', 'max-age=3600, max-stale=1800, min-fresh=3600, only-if-cached');
 	const request =  new Request(url, {
 		method: "GET",
+		mode: 'no-cors',
 		Cache: 'default',
 		Redirect: 'follow',
 		headers
 	});
-	return(	await fetch(request));
+	return(await fetch(request));
 	// globalThis.location.assign(`https://app.ottocratesolver.com/?name=${userInfo.userName}`);k
 };
 
