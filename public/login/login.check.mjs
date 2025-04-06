3907264859440eb8e620a738814b6410732fb82d
Joarell <josephjoarell@gmail.com>
2025-04-05 21:00:16 -0300

    chore (login): check the automatic redirect and updates the wrangler version

--- public/login/login.check.mjs
+++ public/login/login.check.mjs
@@ -70,6 +70,7 @@ async function backEndLoginAuth(userInfo) {
 		mode: 'cors',
 		Redirect: 'follow',
 	});
+	return(await fetch(request));
 	await fetch(request).then(async res => {
 		switch(res.status){
 			case 200:
