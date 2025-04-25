5c3998bca626b43875e5b1fc15fe975caa9f53ac
Joarell <josephjoarell@gmail.com>
2025-04-25 17:26:00 -0300

    login test

--- public/login/login.check.mjs
+++ public/login/login.check.mjs
@@ -82,6 +82,7 @@ async function backEndLoginAuth(userInfo) {
 	});
 	await fetch(request).then(async res => {
 		switch(res.status) {
+			console.log(res);
 			case 200:
 				return(globalThis.location.assign('https://app.ottocratesolver.com'));
 			case 401:
