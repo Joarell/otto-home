9dfd50f46b10dadcb6086701b13edaf7ad9a7b0e
Joarell <josephjoarell@gmail.com>
2025-04-25 13:03:14 -0300

    feat (update): worker/wrangler version updated and login url

--- public/login/login.check.mjs
+++ public/login/login.check.mjs
@@ -30,7 +30,7 @@ export function loginInto() {
 
 
 async function takeLogin(userLogin) {
-	const url = `https://app.ottocratesolver.com/api/v1/boot/login`;
+	const url = `/api/v1/boot/login`;
 
 	if (confirm("This USER is already logged in. Would you like to take it?")) {
 		await fetch(url, {
@@ -67,7 +67,7 @@ async function setLogin(info, userData) {
 
 
 async function backEndLoginAuth(userInfo) {
-	const url = 'https://app.ottocratesolver.com/api/v1/login';
+	const url = `/api/v1/boot/login`;
 	const login = btoa(userInfo.userName + ':' + userInfo.passPhrase);
 
 	const request =  new Request(url, {
